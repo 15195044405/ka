89c4703939f2506cc1ccb8994235b75d5ed709f9
zavier <1262660796@qq.com>
2017-05-05 10:19:40 +0800

    happy

--- application/database.php
+++ application/database.php
@@ -13,15 +13,15 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '139.129.128.42',
     // 数据库名
-    'database'        => '',
+    'database'        => 'ka',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'zavier',
     // 密码
-    'password'        => '',
+    'password'        => 'zavier',
     // 端口
-    'hostport'        => '',
+    'hostport'        => '3326',
     // 连接dsn
     'dsn'             => '',
     // 数据库连接参数
@@ -29,7 +29,7 @@ return [
     // 数据库编码默认采用utf8
     'charset'         => 'utf8',
     // 数据库表前缀
-    'prefix'          => '',
+    'prefix'          => 'ka_',
     // 数据库调试模式
     'debug'           => true,
     // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
